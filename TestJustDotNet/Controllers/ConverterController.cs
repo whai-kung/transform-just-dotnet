07ec068b5957b91bac33da157a62112ecf75a7de
whai kung <n.ratanasuwan@gmail.com>
2021-07-16 23:38:11 +0700

    initial project

---
+++ TestJustDotNet/Controllers/ConverterController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using JUST;
+using Microsoft.AspNetCore.Mvc;
+//using Newtonsoft.Json;
+using TestJustDotNet.Models;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace TestJustDotNet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConverterController : Controller
+    {
+        [HttpPost("ToJust")]
+        public IActionResult ToJustDotNet(RequestModel request)
+        {
+            string transformedString;
+            try
+            {
+                if (string.IsNullOrEmpty(request.Input) || string.IsNullOrEmpty(request.Output))
+                {
+                    var transform = JsonSerializer.Serialize(request.Transformer);
+                    var source = JsonSerializer.Serialize(request.Source);
+                    transformedString = JsonTransformer.Transform(transform, source);
+                }
+                else
+                {
+                    var input = Regex.Replace(request.Input, "(\"(?:[^\"\\\\]|\\\\.)*\")|\\s+", "$1");
+                    var output = Regex.Replace(request.Output, "(\"(?:[^\"\\\\]|\\\\.)*\")|\\s+", "$1");
+                    transformedString = JsonTransformer.Transform(output, input);
+                }
+                return Ok(transformedString);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ TestJustDotNet/Models/RequestModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TestJustDotNet.Models
+{
+    public class RequestModel
+    {
+        public object Source { get; set; }
+        public object Transformer { get; set; }
+        public string Input { get; set; }
+        public string Output { get; set; }
+    }
+}
